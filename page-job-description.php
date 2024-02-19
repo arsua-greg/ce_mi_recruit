85c97b5944623122569cc007ba98561c392a76dc
edselnombrado <edcel@bpoc.co.jp>
2024-02-19 15:17:35 +0800

    job descriotion page need to fix table overflow-x auto

---
+++ page-job-description.php
@@ -0,0 +1,202 @@
+<?php get_header(); ?>
+<section class="sec_JD">
+    <div class="wrapper">
+        <img class="butterfly" src="<?php echo get_template_directory_uri() ?>/release/image/butterfly_1.svg" alt="">
+        <img class="butterfly" src="<?php echo get_template_directory_uri() ?>/release/image/butterfly_2.svg" alt="">
+        <div class="l-wrap">
+            <h3 class="sect_title title">募集要項</h3>
+            <p class="sect_para">HOME / 募集要項</p>
+            <div class="container">
+                <p class="para fpara">本園のみつばち保育園は港区という都会の中のオアシスのような自然に恵まれ、とても環境の良いところに保育園があります。0.12歳児だけの小さな保育園ですが、お庭があり毎日戸外で遊びます。少人数の特性を生かし、子ども一人ひとりの思いとしっかりと向きあい、子どもたちが自分であそびをみつけて自ら関わることを大事に保育をしています。
+                    民間委託をうけ2009年4月に開園した西池袋そらいろ保育園は2019年に新園舎になり、木のぬくもりが心地よい園舎、土、水、虫など自然に触れ四季を感じることができる園庭があります。
+                </p>
+                <p class="para lpara">子どもたちに丁寧にかかわるためには職員も生き生きと働ける場がとても重要と考えています。社会福祉法人みつばち会は労働条件を整備し働きつづけられる職場を目指しています。
+                </p>
+
+                <div class="tblettle-con">
+                    <div class="ttle">
+                        <h3 class="fttle">みつばち保育園 </h3>
+                        <h3 class="lttle"> 保育時間 7:15-19:15</h3>
+                    </div>
+                </div>
+
+                <div class="table-container">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>仕事内容</th>
+                                <th>勤務時間<br>
+                                    8時間拘束7時間実働</th>
+                                <th>基本給(特業手当含む)<br>
+                                    1年目 経験年数考慮あり</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="ftd">
+                                    <p>保育士</p>
+                                </td>
+                                <td>クラス担任、保育業務全般</td>
+                                <td>7:00 - 19:30<br>
+                                    (内7時間実働シフト制)</td>
+                                <td>208,000</td>
+                            </tr>
+                            <tr>
+                                <td class="ftd">
+                                    <p>保育士</p>
+                                </td>
+                                <td>園児の給食調理と提供、<br>
+                                    栄養管理、食育活動</td>
+                                <td>8:30 - 16:30<br>
+                                    9:00 - 17:00<br>
+                                    9:15 - 17:15</td>
+                                <td>201,000</td>
+                            </tr>
+                            <tr>
+                                <td class="ftd">
+                                    <p>調理師</p>
+                                </td>
+                                <td>園児の給食調理、提供、<br>
+                                    食育活動</td>
+                                <td>8:30 - 16:30<br>
+                                    9:00 - 17:00<br>
+                                    9:15 - 17:15</td>
+                                <td>195,000</td>
+                            </tr>
+                            <tr>
+                                <td class="ftd">
+                                    <p>看護師</p>
+                                </td>
+                                <td>園児、職員の健康管理、<br>
+                                    衛生管理、保健指導</td>
+                                <td>7:00 - 17:00<br>
+                                    (基本)</td>
+                                <td>221,000</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+
+                <div class="tblettle-con tblettl2">
+                    <div class="ttle">
+                        <h3 class="fttle fttle2">みつばち保育園 </h3>
+                        <h3 class="lttle lttle2"> 保育時間 7:15-19:15</h3>
+                    </div>
+                </div>
+
+                <div class="table-container">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>仕事内容</th>
+                                <th>勤務時間<br>
+                                    8時間拘束7時間実働</th>
+                                <th>基本給(特業手当含む)<br>
+                                    1年目 経験年数考慮あり</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="ftd2">
+                                    <p>保育士</p>
+                                </td>
+                                <td>クラス担任、保育業務全般</td>
+                                <td>7:00 - 20:30<br>
+                                    (シフト制)</td>
+                                <td>208,000<br>
+                                    +5000 (そらいろ手当)</td>
+                            </tr>
+                            <tr>
+                                <td class="ftd2">
+                                    <p>保育士</p>
+                                </td>
+                                <td>園児の給食調理と提供、<br>
+                                    栄養管理、食育活動</td>
+                                <td></td>
+                                <td>201,000<br>
+                                    +5000 (そらいろ手当)</td>
+                            </tr>
+                            <tr>
+                                <td class="ftd2">
+                                    <p>調理師</p>
+                                </td>
+                                <td>園児の給食調理、提供、<br>
+                                    食育活動</td>
+                                <td></td>
+                                <td>195,000<br>
+                                    +5000 (そらいろ手当)</td>
+                            </tr>
+                            <tr>
+                                <td class="ftd2">
+                                    <p>看護師</p>
+                                </td>
+                                <td>園児、職員の健康管理、<br>
+                                    衛生管理、保健指導</td>
+                                <td>7:00 - 17:00<br>
+                                    (基本)</td>
+                                <td>221,000 <br>
+                                    +5000 (そらいろ手当)</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="tblettle-con">
+                    <h3 class="lhdr">共通 </h3>
+                </div>
+
+                <div class="table-container">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    その他手当
+                                </td>
+                                <td>被服費800円、処遇Ⅲ8000円、キャリアアップ手当223,000円(2023年実績、年度末支給)、交通費(実費)</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    賞与
+                                </td>
+                                <td>3.6ヵ月(2023年度実績)</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    休暇
+                                </td>
+                                <td>年次有給休暇15日、健康管理休暇12日、夏期休暇6日</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    休み
+                                </td>
+                                <td>日、祝日、年末年始(12/29～1/3)</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    加入保険等
+                                </td>
+                                <td>健康、厚生、雇用、退職共済</td>
+                            </tr>
+                            <tr>
+                                <td class="flexp">
+                                    <p>その他</p>
+                                </td>
+                                <td>・産休、育休制度あり <br>
+                                    ・若い職員から子育てしながら働いていいる職員など年齢幅がひろいです。<br>
+                                    ・新人教育、育成、研修あり<br>
+                                    ・職員平均年数　8年<br>
+                                    ・残業ほぼなし</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
