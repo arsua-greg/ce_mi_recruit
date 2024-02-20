ebda1dc53a32719255e126b07f256afd56b1bac9
arsuagreg <gregarsua@bpoc.co.jp>
2024-02-20 10:35:53 +0800

    remove extra spacings

--- front-page.php
+++ front-page.php
@@ -33,7 +33,7 @@
                             'post_type' => 'post',
                             'post_status' => 'publish',
                             'paged' => $paged,
-                            'posts_per_page' => 2
+                            'posts_per_page' => 10
                         );
 
                         $the_query = new WP_Query($args); ?>
